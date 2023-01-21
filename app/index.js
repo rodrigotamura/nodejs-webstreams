eacade6eb5ba0e34374adc3e9544edfec508b471
Rodrigo Tamura <rodrigo.tamura@ifood.com.br>
2023-01-21 18:25:25 -0300

    feat: converting requested data in handleable JSON

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ async function consumeAPI(signal) {
   let counter = 0
   const reader = response.body
     .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON())
     .pipeTo(new WritableStream({
       write(chunk) {
         console.log(++counter, 'chunk', chunk)
@@ -14,6 +15,30 @@ async function consumeAPI(signal) {
     }))
 
     return reader
+}
+
+// this function will ensure in a case where two chunks arrive in a single
+// streaming it will convert correctly in JSON
+// given:{}\n{}
+// must:
+//    {}
+//    {}
+function parseNDJSON() {
+  let ndjsonBuffer = ''
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk
+      const items = ndjsonBuffer.split('\n')
+      items.slice(0, -1)
+        .forEach(item => controller.enqueue(JSON.parse(item)))
+
+      ndjsonBuffer = items[items.length -1]
+    },
+    flush(controller) {
+      if(!ndjsonBuffer) return;
+      controller.enqueue(JSON.parse(ndjsonBuffer))
+    }
+  })
 }
 
 const abortController = new AbortController()
